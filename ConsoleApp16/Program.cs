6a3237b8ff134c35273e264b392a10ed5f16841a
alex <melnikov1991@bk.ru>
2020-09-14 15:19:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var lst = "dsfgsdfgd".ToList();
+
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item);
+
+            }
+
+
+           
+
+        }
+    }
+}
